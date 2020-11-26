641cf9c34f1010c8318e7f845148a92124059f29
Mike Mwanje <mwanjemike767@gmail.com>
2020-11-26 15:45:16 +0300

    Schema level validation

--- models/user.js
+++ models/user.js
@@ -3,33 +3,61 @@ const mongoose = require('mongoose');
 const { Schema } = mongoose;
 
 const userSchema = new Schema({
-  firstname: {
-    type: String,
-    trim: true,
-  },
-  lastname: {
-    type: String,
-    trim: true,
-  },
-  username: {
-    type: String,
-    trim: true,
-  },
-  password: {
-    type: String,
-    trim: true,
-  },
   email: {
     type: String,
     trim: true,
+    required: [true, 'email is required'],
+    unique: true,
+    validate: {
+      validator(v) {
+        // eslint-disable-next-line no-useless-escape
+        return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid email!`,
+    },
   },
   phonenumber: {
     type: String,
     trim: true,
+    unique: true,
+    validate: {
+      validator(v) {
+        // Format is 256-774-123456
+        return /\d{3}-\d{3}-\d{6}/.test(v);
+      },
+      message: (props) => `${props.value} is not a valid phone number!`,
+    },
+  },
+  firstname: {
+    type: String,
+    trim: true,
+    required: true,
+    min: [2, 'first name too short'],
+    max: 20,
+  },
+  lastname: {
+    type: String,
+    trim: true,
+    min: [2, 'last name too short'],
+    max: 25,
+  },
+  username: {
+    type: String,
+    trim: true,
+    required: true,
+    min: [2, 'username too short'],
+    max: 25,
+  },
+  password: {
+    type: String,
+    trim: true,
+    required: [true, 'password is required'],
   },
   usercategory: {
     type: String,
     trim: true,
+    required: [true, 'user category is required'],
+    enum: ['Institution', 'Mentor', 'University Student', 'Secondary Student'],
   },
 });
 
