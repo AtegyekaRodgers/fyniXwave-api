c709300227c88830c54050622cf63844caaaadf7
Mike Mwanje <mwanjemike767@gmail.com>
2020-12-02 07:23:08 +0300

    test helper before

--- test/test_helper.js
+++ test/test_helper.js
@@ -1,17 +1,11 @@
 const {
   before, after,
 } = require('mocha');
-const { dbConnect, dbClose } = require('../config/db');
+const { dbConnect } = require('../config/db');
 
 before((done) => {
   // Connecting to mock database
   dbConnect()
-    .then(() => done())
-    .catch((err) => done(err));
-});
-
-after((done) => {
-  dbClose()
     .then(() => done())
     .catch((err) => done(err));
 });
