db60b89f8e63f0115239997eb6fc7c053848818e
Mike Mwanje <mwanjemike767@gmail.com>
2020-12-03 10:43:38 +0300

    auth login secret

--- test/auth.js
+++ test/auth.js
@@ -8,7 +8,7 @@ const {
 
 const app = require('../app');
 const { dbConnect, dbClose } = require('../config/db');
-require('../config/jwt');
+const secret = require('../config/jwt');
 
 describe('auth tests', () => {
   before((done) => {
