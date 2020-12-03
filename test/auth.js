623a4ca5bccdc7c5551ddcab576479bef1d60e5e
Mike Mwanje <mwanjemike767@gmail.com>
2020-12-03 10:25:46 +0300

    testing auth tests 7

--- test/auth.js
+++ test/auth.js
@@ -48,10 +48,8 @@ describe('auth tests', () => {
         const { body, status } = res;
         // Checking for needed return data
         expect(body).to.be.an('object');
-        body[0].should.have.property('token');
-        body[0].should.have.property('user');
-        expect(body).to.contain.deep.property('token', 'token not sent');
-        expect(body).to.contain.deep.property('user', 'user details not sent');
+        expect(body[0]).to.contain.deep.property('token', 'token not sent');
+        expect(body[0]).to.contain.deep.property('user', 'user details not sent');
         expect(status).to.equal(200);
         done();
       })
