cc537743735f80df0af43d030ffe9acf89c13ab7
Mike Mwanje <mwanjemike767@gmail.com>
2021-01-13 18:57:19 +0300

    test

--- test/user.js
+++ test/user.js
@@ -43,8 +43,8 @@ describe('user tests', () => {
       .catch((err) => done(err));
   });
 
-  // login to get tokens
-  const { token } = (done) => {
+  // login to get details
+  const { user, token, message } = (done) => {
     request(app)
       .post('/auth/login')
       .send({
@@ -52,20 +52,22 @@ describe('user tests', () => {
         password: '*******',
       })
       .then(() => done())
-      .catch((err) => done(err));
+      .catch((err) => {
+        console.log(err);
+        done(err);
+      });
   };
-  console.log(token);
+  console.log({ user, token, message });
   // Adds fields of interest
-  it('create users', (done) => {
-    request(app)
-      .post('/user/interests/')
-      .send(['Social Media', 'Programming', 'Business'])
-      .set('Authorization', `Bearer ${token}`)
-      .then((res) => {
-        expect(res.body.message).to.equal('User interests added');
-        expect(res.status).to.equal(204);
-        done();
-      })
-      .catch((err) => done(err));
-  });
+  //   it('adds fields of interest', (done) => {
+  //     request(app)
+  //       .post(`/user/interests/?id=${user._id}`)
+  //       .send('5ffefcd99327cdc1330fa69f, 5ffefc79747ae9c0b3152cc2, 5ffefccd9327cdc1330fa69e')
+  //       .set('Authorization', `Bearer ${token}`)
+  //       .then((res) => {
+  //         expect(res.status).to.equal(204);
+  //         done();
+  //       })
+  //       .catch((err) => done(err));
+  //   });
 });
