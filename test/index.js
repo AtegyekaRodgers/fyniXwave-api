2d103be7f731202c48d73bd8a41711a46fa31f21
Mike Mwanje <mwanjemike767@gmail.com>
2021-01-21 11:55:53 +0300

    timeout 10000ms

--- test/index.js
+++ test/index.js
@@ -4,10 +4,8 @@ const request = require('supertest');
 const app = require('../app');
 
 describe('index', () => {
-  it('gives welcome message', async (done) => {
+  it('gives welcome message', async () => {
     const res = await request(app).get('/');
-    expect(res.body.message).to.equal('Welcome to delv api');
     expect(res.status).to.equal(200);
-    done();
   });
 });
