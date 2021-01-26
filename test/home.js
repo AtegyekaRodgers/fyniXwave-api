eb85cc1e2c45b429cef4182ae5c6b687879ca349
Mike Mwanje <mwanjemike767@gmail.com>
2021-01-26 21:56:13 +0300

    Get related content test

---
+++ test/home.js
@@ -0,0 +1,56 @@
+const { expect } = require('chai');
+const request = require('supertest');
+const {
+  before, after, describe, it,
+} = require('mocha');
+
+const app = require('../app');
+const { dbConnect, dbClose } = require('../config/db');
+
+describe('home tests', () => {
+  let token;
+  before((done) => {
+    dbConnect().catch((err) => done(err));
+    request(app)
+      .post('/auth/login')
+      .send({
+        email: 'auth@delv.ac.ug',
+        password: 'newPassword',
+      })
+      .then((res) => {
+        token = res.body.token;
+        expect(res.body).to.contain.property('token');
+        done();
+      })
+      .catch((err) => done(err));
+  });
+  after((done) => {
+    dbClose()
+      .then(() => done())
+      .catch((err) => done(err));
+  });
+
+  // Gives user feed
+  it('gives user feed', (done) => {
+    request(app)
+      .get('/home')
+      .set('Authorization', `Bearer ${token}`)
+      .then((res) => {
+        expect(res.status).to.equal(200);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+
+  // Returns related content of interests
+  it('returns related content of interests', (done) => {
+    request(app)
+      .get('/home/relatedContent')
+      .set('Authorization', `Bearer ${token}`)
+      .then((res) => {
+        expect(res.status).to.equal(200);
+        done();
+      })
+      .catch((err) => done(err));
+  });
+});
